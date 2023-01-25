feafe5051666f9aea44867159fc8f63c4f357a0c
Pablo Penalva <penalva.pablo@gmail.com>
2023-01-25 13:18:44 -0300

    fixes

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -7,8 +7,8 @@ const NavMobile2 = ({ mobileNavFn }) => {
     <nav class="bg-secondary h-screen flex items-center justify-center ">
       <ul class="h-[100%] max-h-[300px] flex flex-col items-center text-center justify-around">
         <li><a class="text-primary font-sans font-bold text-navLinks" href="/">Inicio</a></li>
-        <li><a class="text-primary font-sans font-bold text-navLinks " href="/">Consulta</a></li>
-        <li><a class="text-primary font-sans font-bold text-navLinks " href="">Sobre mi</a></li>
+        <li><a class="text-primary font-sans font-bold text-navLinks " href="/#consulta">Consulta</a></li>
+        <li><a class="text-primary font-sans font-bold text-navLinks " href="/about">Sobre mi</a></li>
         <li><a class="text-primary font-sans font-bold text-navLinks " href="/metodo">Método</a></li>
         <li><a class="text-primary font-sans font-bold text-navLinks " href="/preguntas">Preguntas Frecuentes</a></li>
         <li><a class="text-primary font-sans font-bold text-navLinks " href="#contacto" onClick={mobileNavFn}>Contacto</a></li>
