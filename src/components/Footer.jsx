5c9a8483cf60d3eff32d8191bfebfa06d9a6a461
Pablo Penalva <penalva.pablo@gmail.com>
2023-01-27 10:17:28 -0300

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,22 +9,22 @@ const Footer = () => {
     <footer id='contacto' className=" bg-primary">
       <h2 className="font-sans text-title_m text-secondary font-bold text-center pt-[30px] pb-12 tracking-wide">Contacto</h2>
       <div className='w-fit h-[200px] flex flex-col items-start justify-around mx-auto mb-[50px]'>
-        <div className='flex gap-5'>
+        <div className='flex gap-5 items-center'>
           <FontAwesomeIcon icon={faPhone} className='h-[40px] text-secondary text-end justify-items-end' />
           <p className='text-secondary text-paragraph font-sans font-semibold'>
             +34607548117
           </p>
         </div>
-        <div className='flex gap-5'>
+        <div className='flex gap-5 items-center'>
           <FontAwesomeIcon icon={faWhatsapp} className='h-[40px] text-secondary text-end justify-items-end' />
-          <button className='text-secondary text-paragraph font-sans font-semibold'>
-            <a href='https://api.whatsapp.com/send?phone=491785279833' rel='noreferrer' target="_blank">+34607548117</a>
+          <button className='text-secondary text-paragraph font-sans font-semibold '>
+            <a href='https://api.whatsapp.com/send?phone=491785279833' rel='noreferrer' target="_blank" className='hover:text-[20px] duration-100'>+34607548117</a>
 
           </button>
         </div>
-        <div className='flex gap-5'>
+        <div className='flex gap-5 items-center'>
           <FontAwesomeIcon icon={faEnvelope} className='h-[40px] text-secondary text-end justify-items-end' />
-          <p className='text-secondary text-paragraph font-sans font-semibold'>
+          <p className='text-secondary text-paragraph font-sans font-semibold '>
             cinthnadim@gmail.com
           </p>
         </div>
